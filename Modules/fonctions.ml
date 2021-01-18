d897572b0956faca7411d4f599c1f6d203870d71
Gaetan Frejoux <gaetanfrejoux@gmail.com>
2021-01-18 19:14:00 +0100

    Add files via upload

---
+++ Modules/fonctions.ml
@@ -0,0 +1,8 @@
+(*open*)
+open Btree;;
+open Bst;;
+
+(*use*)
+#load "btree.cmo";;
+#load "bst.cmo";;
+
--- projet.ml
+++ projet.ml
@@ -1,8 +1,45 @@
 (*Projet d'algorithmique et Programmation*)
 (*Auteur : Fréjoux Gaëtan && Niord Mathieu*)
 
+(*directory*)
+#directory "Modules";;
+
+(*open*)
+open Btree;;
+open Bst;;
+
+(*use*)
+#load "btree.cmo";;
+#load "bst.cmo";;
 
 (*1 Arbres binaires de recherche*)
+
+let rec height(tree : 'a bs_tree) : int =
+  if(isEmpty(tree) || (isEmpty(lson(tree))&&isEmpty(rson(tree))))
+  then 0
+  else 1+max (height(lson(tree))) (height(rson(tree)))
+;;
+
+
+let bst_rnd_create(n : int) : int bs_tree =
+  let list : int list ref= ref [] in
+  for i = 0 to n do
+    list := (Random.int 10000000)::(!list);
+  done;
+  bst_lbuild(!list)
+;;
+
+
+let equilibre(n : int) : int =
+  let somme : int ref = ref 0 and
+      t : int bs_tree ref = ref (empty()) in
+  for i = 0 to n do
+    t := bst_rnd_create(10000);
+    somme := !somme + abs (height(lson(!t)) - height(rson(!t)));
+  done;
+  (!somme)/n
+;;
+
 
 
 (*1.1*)
